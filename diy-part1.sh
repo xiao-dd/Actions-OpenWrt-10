56a909e85803dca97509436772cddc4d9df9beba
xiao-dd <59915536+xiao-dd@users.noreply.github.com>
2023-01-22 15:21:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#sed -i '$a src-git smpackage https://github.com/shidahuilang/openwrt-package' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/shidahuilang/openwrt-package' feeds.conf.default
